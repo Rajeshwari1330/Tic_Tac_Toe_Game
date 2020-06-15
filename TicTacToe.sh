af8065af865ae66035cc29895dc2aa667a3a4fb2
misti <yallalraj1307@gmail.com>
2020-06-15 12:04:17 +0530

    ADDED: function for assigning X and 0 letters for player and computer.

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -3,18 +3,34 @@ echo "Welcome to the TIC TAC TOE GAME SIMULATOR"
 
 function createBoardReset()
 {
-	board=( " " 1 2 3 4 5 6 7 8 9 )
-	echo "Game is Starting now........."
+        board=( " " 1 2 3 4 5 6 7 8 9 )
+        echo "Game is Starting now........."
 }
 
 function createBoard()
 {
-	echo "${board[1]} | ${board[2]} | ${board[3]}"
-	echo "-----------"
-	echo "${board[4]} | ${board[5]} | ${board[6]}"
-	echo "-----------"
-	echo "${board[7]} | ${board[8]} | ${board[9]}"
-	
+        echo "${board[1]} | ${board[2]} | ${board[3]}"
+        echo "-----------"
+        echo "${board[4]} | ${board[5]} | ${board[6]}"
+        echo "-----------"
+        echo "${board[7]} | ${board[8]} | ${board[9]}"
+ 
+}
+
+
+function assignValue()
+{
+value=$((RANDOM%2))
+if(($value==0))
+then
+	playerValue=0
+	computerValue=x
+	echo "player is assigned $playerValue and computer is assigned $computerValue"
+else
+	playerValue=x
+	computerValue=0
+	echo "player is assigned $playerValue and computer is assigned $computerValue"
+fi
 }
 
 createBoard
@@ -22,3 +38,5 @@ createBoardReset
 echo "Setting up the board and creating"
 sleep 1
 createBoard
+sleep 1
+assignValue
